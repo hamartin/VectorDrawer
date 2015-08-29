939dce953d99f1db5fdc48f2448668fe0f2db7d6
Hans Åge Martinsen <hamartin@moshwire.com>
2015-08-29 23:51:19 +0200

    Added the option to rotate around a "new null point"

--- inc/point.h
+++ inc/point.h
@@ -13,9 +13,9 @@ public:
     Point(point_t *p);
     ~Point();
     point_t *getPoint();
-    void rotate(const int degrees);
-    void rotate(const float degrees);
-    void rotate(const double degrees);
+    void rotate(const int degrees, point_t *origin = NULL);
+    void rotate(const float degrees, point_t *origin = NULL);
+    void rotate(const double degrees, point_t *origin = NULL);
 private:
     point_t *p;
 };
--- src/point.cpp
+++ src/point.cpp
@@ -19,21 +19,26 @@ point_t *Point::getPoint()
     return p;
 }
 
-void Point::rotate(const int degrees)
+void Point::rotate(const int degrees, point_t *origin)
 {
-    Point::rotate((double)degrees);
+    Point::rotate((double)degrees, origin);
 }
 
-void Point::rotate(const float degrees)
+void Point::rotate(const float degrees, point_t *origin)
 {
-    Point::rotate((double)degrees);
+    Point::rotate((double)degrees, origin);
 }
 
-void Point::rotate(const double degrees)
+void Point::rotate(const double degrees, point_t *origin)
 {
     double radian = (degrees * M_PI)/180;
     double s = sin(radian);
     double c = cos(radian);
-    p->x = (int)(p->x * c + p->y * -s);
-    p->y = (int)(p->x * s + p->y * c);
+    if(origin) {
+        p->x = (int)((p->x - origin->x) * c + (p->y - origin->y) * -s);
+        p->y = (int)((p->y - origin->y) * s + (p->y - origin->y) * c);
+    } else {
+        p->x = (int)(p->x * c + p->y * -s);
+        p->y = (int)(p->x * s + p->y * c);
+    }
 }
