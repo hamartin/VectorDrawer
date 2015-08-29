86a9430065fc89c5c976d0c3268b2df5c66a789d
Hans Åge Martinsen <hamartin@moshwire.com>
2015-08-29 23:44:47 +0200

    Added some methods to the point class.

--- inc/point.h
+++ inc/point.h
@@ -5,12 +5,17 @@
 #ifndef __POINT_H__
 #define __POINT_H__
 
+#include <cmath>
 #include "variables.h"
 
 class Point {
 public:
     Point(point_t *p);
     ~Point();
+    point_t *getPoint();
+    void rotate(const int degrees);
+    void rotate(const float degrees);
+    void rotate(const double degrees);
 private:
     point_t *p;
 };
--- src/point.cpp
+++ src/point.cpp
@@ -13,3 +13,27 @@ Point::~Point()
 {
     delete p;
 }
+
+point_t *Point::getPoint()
+{
+    return p;
+}
+
+void Point::rotate(const int degrees)
+{
+    Point::rotate((double)degrees);
+}
+
+void Point::rotate(const float degrees)
+{
+    Point::rotate((double)degrees);
+}
+
+void Point::rotate(const double degrees)
+{
+    double radian = (degrees * M_PI)/180;
+    double s = sin(radian);
+    double c = cos(radian);
+    p->x = (int)(p->x * c + p->y * -s);
+    p->y = (int)(p->x * s + p->y * c);
+}
